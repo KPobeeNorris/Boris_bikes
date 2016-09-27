1bfb98ad0d037e6a3064fd2e9e76d69d9eddc725
Frankie Bell <fbell.pro@gmail.com>
2016-09-27 16:30:27 +0100

    updated file as I could not pull

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,8 @@
-describe DockingStation do
+require './lib/docking_station_class.rb'
 
-	
+describe DockingStation do
+  it 'DockingStation to respond_to release_bike method' do
+    expect(release_bike).to eq true
+  end
+
 end
