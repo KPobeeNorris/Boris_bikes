4723d149ed95276048fcaba42cdccc8127d075f6
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-09-28 12:11:20 +0100

    Step 10 - amended working method? to pass rpsec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,19 @@
 require './lib/docking_station_class.rb'
+require './lib/bike.rb'
 
 describe DockingStation do
+  docking_station = DockingStation.new
+  bike = docking_station.release_bike
+  it 'DockingStation to return new instance of Bike class' do
+    expect(bike.class).to eq Bike
+  end
+
+  it 'DockingStation expects bike to be working?' do
+    expect(bike.working?).to eq true
+  end
+
   it { is_expected.to respond_to :release_bike }
-  #refactored to single line syntax
+
 
 end
 
